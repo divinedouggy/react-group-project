e6960e1df9e5f1e5738a5cb373235ae53ba4dd6a
tameramiddlebrooks <105788615+tameramiddlebrooks@users.noreply.github.com>
2022-08-02 18:00:25 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Tamera from './components/Tamera';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+    <Tamera />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Tamera.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Tamera () {
+    return (
+        <div>
+            <h1>Hi, I'm Tamera! 😊</h1>
+            <p className="intro">Below, you'll find a few fun facts about me.</p>
+            <ol>
+                <li className ="aboutme-list"> I enjoy doing freelance photography in my spare time.
+
+                </li>
+
+                <li className ="aboutme-list"> I'd only taken one coding class before being accepted
+                    into Per Scholas.
+                </li>
+                
+                <li className ="aboutme-list"> My favorite color is brown, it's super underrated.
+                </li>
+                
+                <li className ="aboutme-list"> My favorite TV show is the Twilight Zone.
+
+                </li>
+                
+                <li className ="aboutme-list"> I love Pixar movies, my favorite one is Ratatouille.</li>
+
+                <li className ="aboutme-list" >I'm a Pescetarian!</li>
+            </ol>
+        </div>
+    )
+}
+
+export default Tamera;
