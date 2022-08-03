8eb01cbe1c5ac28fb6d849c52ecb2888f05077e6
aduratovic <aduratovic22@gmail.com>
2022-08-02 20:43:27 -0400

    my page update

--- src/components/Ajdin.js
+++ src/components/Ajdin.js
@@ -1,15 +1,17 @@
+import react from "react";
+
 function Ajdin() {
   return (
     <div>
       <h1>Favorites</h1>
-      <p>Favorite Sports:</p>
-      <ul>
+      <p class="bold">Favorite Sports:</p>
+      <ul class="favorites">
         <li>Basketball(pro and collegiate)</li>
         <li>Football(pro and collegiate)</li>
         <li>Soccer(futbol)</li>
       </ul>
-      <p>Favorite Teams:</p>
-      <ul>
+      <p class="bold">Favorite Teams:</p>
+      <ul class="favorites">
         <li>
           Basketball - Los Angeles Lakers/Detroit Pistons/ University Of
           Michigan Wolverines
@@ -17,14 +19,14 @@ function Ajdin() {
         <li>Football - Detroit Lions/University Of Michigan Wolverines</li>
         <li>Soccer(futbol)- FC Barcelona</li>
       </ul>
-      <p>Favorite Music Genre:</p>
-      <ul>
+      <p class="bold">Favorite Music Genre:</p>
+      <ul class="favorites">
         <li>Rap</li>
         <li>Modern Country</li>
         <li>Balkan rap/pop</li>
       </ul>
-      <p>Favorite Artists:</p>
-      <ul>
+      <p class="bold">Favorite Artists:</p>
+      <ul class="favorites">
         <li>Drake/Moneybagg Yo/ Post Malone/Lil Wayne</li>
         <li>Morgan Wallen/Luke Combs</li>
         <li>Jala Brat/Buba Corelli</li>
@@ -33,4 +35,4 @@ function Ajdin() {
   );
 }
 
-export default ajdin;
+export default Ajdin;
