d0298fde2a133f308ffcdf3721e448776e7d0c77
aduratovic <aduratovic22@gmail.com>
2022-08-02 20:11:31 -0400

    my page update

--- src/components/Ajdin.js
+++ src/components/Ajdin.js
@@ -1,12 +1,36 @@
-
-
-
 function Ajdin() {
-    return(
-        <div>
-   
-        </div>
-    )
+  return (
+    <div>
+      <h1>Favorites</h1>
+      <p>Favorite Sports:</p>
+      <ul>
+        <li>Basketball(pro and collegiate)</li>
+        <li>Football(pro and collegiate)</li>
+        <li>Soccer(futbol)</li>
+      </ul>
+      <p>Favorite Teams:</p>
+      <ul>
+        <li>
+          Basketball - Los Angeles Lakers/Detroit Pistons/ University Of
+          Michigan Wolverines
+        </li>
+        <li>Football - Detroit Lions/University Of Michigan Wolverines</li>
+        <li>Soccer(futbol)- FC Barcelona</li>
+      </ul>
+      <p>Favorite Music Genre:</p>
+      <ul>
+        <li>Rap</li>
+        <li>Modern Country</li>
+        <li>Balkan rap/pop</li>
+      </ul>
+      <p>Favorite Artists:</p>
+      <ul>
+        <li>Drake/Moneybagg Yo/ Post Malone/Lil Wayne</li>
+        <li>Morgan Wallen/Luke Combs</li>
+        <li>Jala Brat/Buba Corelli</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Ajdin;
+export default ajdin;
